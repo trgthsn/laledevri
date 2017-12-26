53d1fb7462729612191e9767d9af8c5a2ab1b796
turguthasan <cehasanturgut@outlook.com>
2017-12-26 21:28:48 +0300

    ilk commit

---
+++ src/main/java/laledevri/laladevri/App.java
@@ -0,0 +1,13 @@
+package laledevri.laladevri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
