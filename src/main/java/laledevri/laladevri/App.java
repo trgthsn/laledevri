6451df329485fd140ae303eae355523fa52a58ac
turguthasan <cehasanturgut@outlook.com>
2017-12-26 21:37:35 +0300

    second commit

--- src/main/java/laledevri/laladevri/App.java
+++ src/main/java/laledevri/laladevri/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
+
+int target2 = -5;
+
+
+
+	int num3 = 3;
+
+
+
+final int deneme2;
+
+
+
+	target2 =- num3;  // Noncompliant; target = -3. Is that really what's meant?
         System.out.println( "Hello World!" );
     }
 }
